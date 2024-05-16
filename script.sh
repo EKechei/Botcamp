828dbde73b84e2e300cdb995f2b8ad4b0f8af44a
Edith Cherotich <128794751+EKechei@users.noreply.github.com>
2024-05-16 13:46:02 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
